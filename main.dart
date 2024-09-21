ad26f2faaf770697076bceb26081d7517ee68a7a
Yusra272 <yusraahsankhan1@gmail.com>
2024-09-21 10:23:37 -0700

    assigment5

---
+++ main.dart
@@ -0,0 +1,26 @@
+void main(){
+
+  // 1
+
+//  var marks = 99;
+
+//  if(marks == 99){
+//   print ('pass');
+//  }
+//   else{
+//     print('fail');
+//   }
+
+//   // 2
+
+//   List int = [5,2,4,1,3];
+//   int.sort();
+//   print(int);
+ 
+// 3
+
+// for (var i = 1; i<=10; i++) {
+//   print(i*3);
+// }
+}
+  
